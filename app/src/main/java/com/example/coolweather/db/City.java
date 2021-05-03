62a7ece959911e543973d6e1c9757e693868005f
zhoulu <1516496567@qq.com>
2021-05-03 20:28:19 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,44 @@
+package com.example.coolweather.db;
+
+public class City {
+
+    private int id;
+
+    private String cityName;
+
+    private int CityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return CityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        CityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
